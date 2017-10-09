7f4dc04aa9bf42214cce6e513dcf05101740d84e
pouya-barzegar <poooooyabarzegar@gmail.com>
2017-10-09 17:25:18 +0330

    more admininstrator commands

--- commands.py
+++ commands.py
@@ -14,9 +14,13 @@ logging.basicConfig(filename='./telegbot.log',
 logger = logging.getLogger(__name__)
 
 
+# init redis database
+database = db.MyDB('localhost', 6379, db=1)
+
+
 def start(bot, update):
-    rd = db.redis_obj
-    rd.incr("numOfUsers")
+    rd = database.redis_obj
+    rd.incr("user_count")
 
     update.message.reply_text('خوش آمدید!')
     logger.info("start command used by {} "
@@ -32,5 +36,48 @@ def help(bot, update):
 
 def addButton(bot, update, arg):
     button = arg[0]
-    rd = redis.Redis()
+    rd = database.redis_obj
     rd.sadd('buttons', button)
+
+
+def delButton(bot, update, arg):
+    button = arg[0]
+    rd = database.redis_obj
+    try:
+        rd.srem('buttons', button)
+    except:
+        print("this button doesn't exist")
+
+
+def addAdmin(bot, update, arg):
+    admin_name = arg[0]
+    rd = database.redis_obj
+    if rd.sismember('admin_users', admin_name):
+        try:
+            rd.sadd('admin_users', admin_name)
+        except Exception as e:
+            raise Exception
+
+
+def login(bot, update, arg):
+    password = arg[0]
+    rd = database.redis_obj
+    if password == rd.get("admin_password"):
+        # form_user should be user id
+        rd.sadd("loginusers", update.message.from_user)
+        update.message.reply_text("Login successful")
+    else:
+        update.message.reply_text("Login Failure.\
+                                  wrong password or you're already logged in")
+
+
+def senddoc(bot, update):
+    # TODO: get docs from the user and store them in db
+    pass
+
+
+def set_password(bot, update, arg):
+    rd = database.redis_obj
+    password = arg[0]
+    if rd.sismember("admin_users", update.message.chat_id):
+        rd.set("admin_password", password)
